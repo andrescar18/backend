8d84021619404f22aa3d5a0bec42503b5dcbeb71
andrescar18 <alcarrillop@gmail.com>
2020-03-22 21:21:13 -0500

    Method saveOrUpdate is available

---
+++ src/main/java/com/youtube/demo/controllers/UserController.java
@@ -0,0 +1,60 @@
+package com.youtube.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.youtube.demo.model.User;
+import com.youtube.demo.service.UserService;
+import com.youtube.demo.util.RestResponse;
+	
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	protected UserService userService;
+	
+	protected ObjectMapper mapper;
+	
+	@RequestMapping(value="/saveOrUpdate", method = RequestMethod.POST)
+	public RestResponse saveOrUpdate(@RequestBody String userJson) 
+			throws JsonMappingException, JsonProcessingException {
+		
+		this.mapper = new ObjectMapper();
+		
+		User user = this.mapper.readValue(userJson, User.class);
+		
+		if(!this.validate(user)) {
+			return new RestResponse(HttpStatus.NOT_ACCEPTABLE.value() ,
+					"No están diligenciados los campos obligatorios");
+		}
+		
+		this.userService.save(user);
+		
+		return new RestResponse(HttpStatus.OK.value() ,
+				"Operacion exitosa");
+	}
+	
+	private boolean validate(User user) {
+		boolean isValid = true;
+		if (user.getFirstName() == "" || user.getFirstName() == null) {
+			isValid = false;
+		}
+		if (user.getFirstSurname() == "" || user.getFirstSurname() == null) {
+			isValid = false;
+		}
+		if (user.getAddress() == "" || user.getAddress() == null) {
+			isValid = false;
+		}
+		return isValid;
+		
+	}
+	
+}
